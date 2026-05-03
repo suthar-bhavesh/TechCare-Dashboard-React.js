0b8863266cb704a075c5d35551ea3efc7e328259
Bhavesh Suthar <95290992+suthar-bhavesh@users.noreply.github.com>
2026-05-03 18:13:58 +0530

    Fix: base path updated in vite.config.js and main.jsx, package file

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const basename =
-  import.meta.env.MODE === "production" &&
-  window.location.hostname.includes("github.io")
-    ? "/TechCare-Dashboard-React.js"
-    : "/";
+const basename = window.location.hostname.includes("github.io")
+  ? "/TechCare-Dashboard-React.js"
+  : "/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename={basename}>

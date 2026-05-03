32fc4d0e84f70d34c41dbe89bb73861a627955ef
Bhavesh Suthar <95290992+suthar-bhavesh@users.noreply.github.com>
2026-05-03 18:47:54 +0530

    Fix: base path updated

--- src/main.jsx
+++ src/main.jsx
@@ -13,4 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </BrowserRouter>,
 );
 
-console.log(basename);
+console.log("Current Basename", basename);
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import process from "node:process";
 
 export default defineConfig(({ mode }) => {
-  const isVercel = process.env.VERCEL === "true";
+  const currentMode = mode || "development";
+
+  const env = loadEnv(currentMode, process.cwd(), "");
+
+  const isVercel = env.VERCEL === "1" || env.VERCEL === "true";
+  const isGithubPages = env.IS_GH_PAGES === "true";
+
+  console.log(
+    `Building for: ${currentMode} | isVercel: ${isVercel} | isGH: ${isGithubPages}`,
+  );
 
   return {
     plugins: [react(), tailwindcss()],
-    base:
-      mode === "production" && !isVercel
-        ? "/TechCare-Dashboard-React.js/"
-        : "/",
+    base: isGithubPages && !isVercel ? "/TechCare-Dashboard-React.js/" : "/",
   };
 });
-

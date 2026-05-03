8dde7a9116e8dced68f999e774fbc98a4d12d444
Bhavesh Suthar <95290992+suthar-bhavesh@users.noreply.github.com>
2026-05-03 15:15:14 +0530

    Fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: mode === "production" ? "/TechCare-Dashboard-React.js/" : "/",
+  base:
+    mode === "production"
+      ? import.meta.env.VITE_DEPLOY_TARGET === "gh"
+        ? "/TechCare-Dashboard-React.js/"
+        : "/"
+      : "/",
 }));

feb796477c611b74375dc55ea0b7ecd6c53381e4
Bhavesh Suthar <95290992+suthar-bhavesh@users.noreply.github.com>
2026-05-03 17:25:19 +0530

    Fix: vite.config.js update with correct base

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import process from "node:process";
 
 export default defineConfig(({ mode }) => {
-  const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const isGitHub = env.VITE_DEPLOY_TARGET === "gh";
 
   return {
     plugins: [react(), tailwindcss()],
-    base: isGitHubPages ? "/TechCare-Dashboard-React.js/" : "/",
+
+    base: isGitHub ? "/TechCare-Dashboard-React.js/" : "/",
   };
 });

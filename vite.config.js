9b86c6020069472ae089909a863381afc338a7b1
Bhavesh Suthar <95290992+suthar-bhavesh@users.noreply.github.com>
2026-05-03 17:31:34 +0530

    Fix: base path updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import process from "node:process";
 
+// Sahi: Arrow function ka use kiya
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  const isGitHub = env.VITE_DEPLOY_TARGET === "gh";
+  console.log("Mera mode hai:", mode);
 
   return {
     plugins: [react(), tailwindcss()],
-
-    base: isGitHub ? "/TechCare-Dashboard-React.js/" : "/",
+    base: mode === "production" ? "/TechCare-Dashboard-React.js/" : "/",
   };
 });
